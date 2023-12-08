6a0a0ac869d419bbbb52d5949b8ee4b20a6bdd5f
Jon Seager <jon@sgrs.uk>
2023-12-08 16:30:12 +0000

    feat: add a NixOS module for starting libations

--- flake.nix
+++ flake.nix
@@ -2,31 +2,36 @@
   description = "libations - a web app for viewing cocktail recipes";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     { self
     , nixpkgs
-    , flake-utils
     , ...
     }:
-    flake-utils.lib.eachSystem [
-      "x86_64-linux"
-      "aarch64-linux"
-    ]
-      (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        # Generate a user-friendly version number.
-        version = builtins.substring 0 8 self.lastModifiedDate;
-      in
-      {
-        overlays.default = _final: _prev: {
-          libations = self.packages.libations;
-        };
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
 
-        packages = rec {
+      pkgsForSystem = system: (import nixpkgs {
+        inherit system;
+        overlays = [ self.overlays.default ];
+      });
+
+      version = builtins.substring 0 8 self.lastModifiedDate;
+    in
+    {
+      overlays.default = _final: _prev: {
+        libations = self.packages.libations;
+      };
+
+      packages = forAllSystems (system:
+        let
+          pkgs = pkgsForSystem system;
+        in
+        rec {
           libations = pkgs.buildGoModule {
             pname = "libations";
             inherit version;
@@ -36,22 +41,73 @@
             postConfigure = "go generate";
           };
           default = libations;
-        };
+        });
 
-        devShells = {
-          default = pkgs.mkShell {
-            name = "libations";
-            NIX_CONFIG = "experimental-features = nix-command flakes";
-            nativeBuildInputs = with pkgs; [
-              go_1_21
-              go-tools
-              gofumpt
-              gopls
-              hugo
-            ];
-            shellHook = "exec $SHELL";
+      nixosModules = rec {
+        default = libations;
+        libations = { config, lib, pkgs, ... }:
+          let
+            cfg = config.services.libations;
+          in
+          {
+            options = {
+              services.libations = {
+                enable = lib.mkEnableOption "Enables the libations service";
+
+                tailscaleKeyFile = lib.mkOption {
+                  type = lib.types.nullOr lib.types.path;
+                  default = null;
+                  example = "/run/agenix/libations-tsauthkey";
+                  description = lib.mdDoc ''
+                    A file containing a key for Libations to join a Tailscale network.
+                    See https://tailscale.com/kb/1085/auth-keys/.
+                  '';
+                };
+
+                package = lib.mkPackageOptionMD pkgs "libations" { };
+              };
+            };
+
+            config = lib.mkIf cfg.enable {
+              systemd.services.libations = {
+                description = "Libations cocktail recipe viewer";
+                wantedBy = [ "multi-user.target" ];
+                after = [ "network.target" ];
+                environment = {
+                  "XDG_CONFIG_HOME" = "/var/lib/libations/";
+                };
+                serviceConfig = {
+                  DynamicUser = true;
+                  ExecStart = "${cfg.package}/bin/libations";
+                  Restart = "always";
+                  EnvironmentFile = cfg.tailscaleKeyFile;
+                  StateDirectory = "libations";
+                  StateDirectoryMode = "0750";
+                };
+              };
+            };
           };
-        };
-      }
-      );
+
+
+        devShells = forAllSystems (system:
+          let
+            pkgs = pkgsForSystem system;
+          in
+          {
+            default = pkgs.mkShell {
+              name = "libations";
+              NIX_CONFIG = "experimental-features = nix-command flakes";
+              nativeBuildInputs = with pkgs; [
+                go_1_21
+                go-tools
+                gofumpt
+                gopls
+                hugo
+              ];
+              shellHook = "exec $SHELL";
+            };
+          });
+      };
+    };
 }
+
