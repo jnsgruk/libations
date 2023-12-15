a882e36dd11f6810cd8fcd84081cc9b08143de9a
Jon Seager <jon@sgrs.uk>
2023-12-15 09:58:38 +0000

    fix: blur active element on search submit to hide mobile keyboards

--- webui/static/js/search.js
+++ webui/static/js/search.js
@@ -50,7 +50,10 @@ const initNavigationSearch = () => {
     })
   })
 
-  document.querySelector(".p-search-box").addEventListener("submit", e => e.preventDefault())
+  document.querySelector(".p-search-box").addEventListener("submit", e => {
+    e.preventDefault()
+    document.activeElement.blur()
+  })
 
   document.querySelector(".p-navigation__search-overlay").addEventListener("click", closeSearch)
 }
